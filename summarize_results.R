3d76dcd4433b90b62c4b21874762bc4faca80fba
Paul Heisig <pkheisig@gmail.com>
2025-12-28 10:32:06 +0100

    Configure .gitignore to exclude data/plots and update README with detailed plot explanations

--- summarize_results.R
+++
@@ -1,5 +0,0 @@
-library(data.table)
-# Load results
-# In run_analysis.R, results_df was created. Let's recreate a summary.
-# Since results_df is in memory in the R session that finished, I should have saved it.
-# I'll modify run_analysis.R to save the results to a CSV.
